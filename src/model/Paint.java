09a0ab7feb0dd136e16809b21037b53add2b3d80
Lars <snooglecod@gmail.com>
2017-02-03 12:03:34 +0100

    Lagde paint som hoved grafisk klasse

---
+++ src/model/Paint.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author lars
+ */
+public class Paint {
+    public void drawGrid(int width, int height)
+    {
+        for(int t=0 ; t<width; t++)
+        {
+            
+        }
+        for(int t=0; t<height; t++)
+        {
+            
+        }
+    }
+}
